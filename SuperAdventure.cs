357125fb19e32d4151d61bdafc6c610a13e7ae64
creativwhisper <creativewhisperings@gmail.com>
2017-05-08 23:24:17 +0200

    Player Class
    Added Player Class, fixed some label issues.

--- SuperAdventure.cs
+++ SuperAdventure.cs
@@ -8,18 +8,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Engine; // Llamada a la librería de clases donde almacenamos la lógica de juego.
+
 namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
+        // instancia de la clase
+        public Player _player;
+
         public SuperAdventure()
         {
-            InitializeComponent();
+            InitializeComponent(); // Añadido por VS
+
+            _player = new Player(); // Creación de instancia de la clase.
+
+            _player.MaximumHitPoints = 10;
+            _player.CurrentHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.CurrentHitPoints.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+
+
         }
 
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            lblGold.Text = "123";
-        }
+        
+
+        
     }
 }
