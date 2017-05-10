592bca6f6a9d5bc18798cc1e6a1c26da5b9dbec3
creativwhisper <creativewhisperings@gmail.com>
2017-05-10 17:40:05 +0200

    Location en Player
    Añadido propiedad CurrentLocation a la clase Player

--- Engine/World.cs
+++ Engine/World.cs
@@ -8,6 +8,7 @@ namespace Engine
 {
     public static class World
     {
+
         public static readonly List<Item> Items = new List<Item>();
         public static readonly List<Monster> Monsters = new List<Monster>();
         public static readonly List<Quest> Quests = new List<Quest>();
