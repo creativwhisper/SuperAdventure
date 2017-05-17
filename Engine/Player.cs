715ddbaddfc08b124f81627b4b9bcc6a6efce2da
creativwhisper <creativewhisperings@gmail.com>
2017-05-17 18:13:56 +0200

    Guardar/Cargar desde XML
    - Al salir se almacenan datos del jugador en un XML
    - Al cargar juego se carga el XML o se carga un player default si no
    existe o da error.
    - Modificada clase Player para albergar las funciones de salvar/cargar
    - Correcciones menores en Player para meter características que
    faltaban.

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Engine
 {
@@ -19,44 +20,177 @@ namespace Engine
         public List<InventoryItem> Inventory { get; set; }
         public List<PlayerQuest> Quests { get; set; }
 
-        
-
-        public Player(int maximumHitPoints, int currentHitPoints, int gold, int experiencePoints, int level, int currentStrength, int currentDexterity)
-            : base (maximumHitPoints, currentHitPoints)
+        private Player(int currentHitPoints, int maximumHitPoints, int level, int currentStrength, int currentDexterity, int gold, int experiencePoints) : base(currentHitPoints, maximumHitPoints)
         {
-            Gold = gold;
-            ExperiencePoints = experiencePoints;
             Level = level;
             CurrentStrength = currentStrength;
             CurrentDexterity = currentDexterity;
+            Gold = gold;
+            ExperiencePoints = experiencePoints;
+
             Inventory = new List<InventoryItem>();
             Quests = new List<PlayerQuest>();
-            
+        }
+
+        public static Player CreateDefaultPlayer()
+        {
+            Player player = new Player(10, 10, 1, 5, 5, 20, 0);
+            player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
+            player.CurrentLocation = World.LocationByID(World.LOCATION_ID_HOME);
+
+            return player;
+        }
+
+        public static Player CreatePlayerFromXmlString(string xmlPlayerData)
+        {
+            try
+            {
+                XmlDocument playerData = new XmlDocument();
+
+                playerData.LoadXml(xmlPlayerData);
+
+                int currentHitPoints = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentHitPoints").InnerText);
+                int maximumHitPoints = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/MaximumHitPoints").InnerText);
+                int level = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Level").InnerText);
+                int currentStrength = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentStrength").InnerText);
+                int currentDexterity = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentDexterity").InnerText);
+                int gold = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/Gold").InnerText);
+                int experiencePoints = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/ExperiencePoints").InnerText);
+
+                Player player = new Player(currentHitPoints, maximumHitPoints, level, currentStrength, currentDexterity, gold, experiencePoints);
+
+                int currentLocationID = Convert.ToInt32(playerData.SelectSingleNode("/Player/Stats/CurrentLocation").InnerText);
+                player.CurrentLocation = World.LocationByID(currentLocationID);
+
+                foreach (XmlNode node in playerData.SelectNodes("/Player/InventoryItems/InventoryItem"))
+                {
+                    int id = Convert.ToInt32(node.Attributes["ID"].Value);
+                    int quantity = Convert.ToInt32(node.Attributes["Quantity"].Value);
+
+                    for (int i = 0; i < quantity; i++)
+                    {
+                        player.AddItemToInventory(World.ItemByID(id));
+                    }
+                }
+
+                foreach (XmlNode node in playerData.SelectNodes("/Player/PlayerQuests/PlayerQuest"))
+                {
+                    int id = Convert.ToInt32(node.Attributes["ID"].Value);
+                    bool isCompleted = Convert.ToBoolean(node.Attributes["IsCompleted"].Value);
+
+                    PlayerQuest playerQuest = new PlayerQuest(World.QuestByID(id));
+                    playerQuest.IsCompleted = isCompleted;
+
+                    player.Quests.Add(playerQuest);
+                }
+
+                return player;
+            }
+            catch
+            {
+                // If there was an error with the XML data, return a default player object
+                return Player.CreateDefaultPlayer();
+            }
+        }
+
+        // Función para convertir los datos de la ficha de personaje a un archivo XML
+        public string ToXmlString()
+        {
+            // Crea el documento
+            XmlDocument playerData = new XmlDocument();
+
+            // Crea el nodo más alto de la jerarquía del que cuelgan Stats, InventoryItems y PlayerQuests
+            XmlNode player = playerData.CreateElement("Player");
+            playerData.AppendChild(player);
+
+            // Crea el segundo nodo, Stats, de donde colgarán los nodos con los datos.
+            XmlNode stats = playerData.CreateElement("Stats");
+            player.AppendChild(stats);
+
+            // Crea los nodos hijos del de Stats
+            XmlNode currentHitPoints = playerData.CreateElement("CurrentHitPoints");
+            currentHitPoints.AppendChild(playerData.CreateTextNode(this.CurrentHitPoints.ToString()));
+            stats.AppendChild(currentHitPoints);
+
+            XmlNode maximumHitPoints = playerData.CreateElement("MaximumHitPoints");
+            maximumHitPoints.AppendChild(playerData.CreateTextNode(this.MaximumHitPoints.ToString()));
+            stats.AppendChild(maximumHitPoints);
+
+            XmlNode currentStrength = playerData.CreateElement("CurrentStrength");
+            currentStrength.AppendChild(playerData.CreateTextNode(this.CurrentStrength.ToString()));
+            stats.AppendChild(currentStrength);
+
+            XmlNode currentDexterity = playerData.CreateElement("CurrentDexterity");
+            currentDexterity.AppendChild(playerData.CreateTextNode(this.CurrentDexterity.ToString()));
+            stats.AppendChild(currentDexterity);
+
+            XmlNode level = playerData.CreateElement("Level");
+            level.AppendChild(playerData.CreateTextNode(this.Level.ToString()));
+            stats.AppendChild(level);
+
+            XmlNode gold = playerData.CreateElement("Gold");
+            gold.AppendChild(playerData.CreateTextNode(this.Gold.ToString()));
+            stats.AppendChild(gold);
+
+            XmlNode experiencePoints = playerData.CreateElement("ExperiencePoints");
+            experiencePoints.AppendChild(playerData.CreateTextNode(this.ExperiencePoints.ToString()));
+            stats.AppendChild(experiencePoints);
+
+            XmlNode currentLocation = playerData.CreateElement("CurrentLocation");
+            currentLocation.AppendChild(playerData.CreateTextNode(this.CurrentLocation.ID.ToString()));
+            stats.AppendChild(currentLocation);
+
+            // Crea el nodo InventoryItems del que colgarán todos los nodos con los objetos del inventario
+            XmlNode inventoryItems = playerData.CreateElement("InventoryItems");
+            player.AppendChild(inventoryItems);
+
+            // Crea un nodo InventoryItem por cada objeto en el inventario del jugador.
+            foreach (InventoryItem item in this.Inventory)
+            {
+                XmlNode inventoryItem = playerData.CreateElement("InventoryItem");
+
+                XmlAttribute idAttribute = playerData.CreateAttribute("ID");
+                idAttribute.Value = item.Details.ID.ToString();
+                inventoryItem.Attributes.Append(idAttribute);
+
+                XmlAttribute quantityAttribute = playerData.CreateAttribute("Quantity");
+                quantityAttribute.Value = item.Quantity.ToString();
+                inventoryItem.Attributes.Append(quantityAttribute);
+
+                inventoryItems.AppendChild(inventoryItem);
+            }
+
+            // Crea el nodo hijo PlayerQuests del que colgarán los nodos de las misiones del jugador
+            XmlNode playerQuests = playerData.CreateElement("PlayerQuests");
+            player.AppendChild(playerQuests);
+
+            // Crea un nodo PlayerQuest para cada quest que haya activa en la lista del jugador.
+            foreach (PlayerQuest quest in this.Quests)
+            {
+                XmlNode playerQuest = playerData.CreateElement("PlayerQuest");
+
+                XmlAttribute idAttribute = playerData.CreateAttribute("ID");
+                idAttribute.Value = quest.Details.ID.ToString();
+                playerQuest.Attributes.Append(idAttribute);
+
+                XmlAttribute isCompletedAttribute = playerData.CreateAttribute("IsCompleted");
+                isCompletedAttribute.Value = quest.IsCompleted.ToString();
+                playerQuest.Attributes.Append(isCompletedAttribute);
+
+                playerQuests.AppendChild(playerQuest);
+            }
+
+            return playerData.InnerXml; // Retorna el documento XML en un string, para poder salvarlo a disco.
         }
 
         public int SetNewLevel()
         {
             Level++;
+            CurrentStrength++;
+            CurrentDexterity++;
+            MaximumHitPoints += 5;
             return Level;
 
-        }
-
-        public int SetNewDexterity()
-        {
-            CurrentDexterity++;
-            return CurrentDexterity;
-        }
-
-        public int SetNewStrength()
-        {
-            CurrentStrength++;
-            return CurrentStrength;
-        }
-
-        public int SetNewMaxHealth()
-        {
-            MaximumHitPoints += 5;
-            return MaximumHitPoints;
         }
 
         public bool HasRequiredItemToEnterThisLocation(Location location)
